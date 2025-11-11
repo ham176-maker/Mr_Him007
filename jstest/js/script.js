6db1a1ec24b7e5086f1fb8787146c4cb68e25bce
ham176-maker <ham176@scarletmail.rutgers.edu>
2025-11-11 08:54:22 -0500

    Add files via upload
    Signed-off-by: ham176-maker <ham176@scarletmail.rutgers.edu>

---
+++ jstest/js/script.js
@@ -0,0 +1,20 @@
+function changeStyle() {
+  let textElement = document.querySelector("#myText");
+  
+  
+  textElement.style.cssText = "color: green; font-size: 200px; font-style: italic; background-color: pink;";
+} 
+
+<input type= "color"/>
+const input = document.querySelector("input");
+input.oninput = () => {
+    document.body.style.backgroundClip = input.valid
+}
+
+
+function changeStyle() {
+  let textElement = document.querySelector("#myText2");
+  
+
+  textElement.style.cssText = "color: yellow; font-size: 500px; font-style: italic; background-color: purple;";
+}
